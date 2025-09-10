5395fe48f61cfc267a1602c8e2a17be4b9dc67f9
Mehrubon <123616994+meha001@users.noreply.github.com>
2025-09-10 20:53:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 from currency_converter import CurrencyConverter
 from telebot import types
 
-# Создаем бота
+# Создаем бота.
 bot = telebot.TeleBot('Token к боту')
 # Переменная для суммы
 amount = 0
